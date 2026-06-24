8d72b53a8a4ead82bcec2cc9859e9664a1f6efc0
Milan <work.milandave@gmail.com>
2026-06-24 16:20:18 +0100

    fix: User model typo fix

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db.base import Base
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
     
     id: Mapped[int] = mapped_column(
